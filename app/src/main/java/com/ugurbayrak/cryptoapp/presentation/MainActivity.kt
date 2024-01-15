cabf4380f0527176f7ed1b2ba280c275cff4c2a9
Uğur Bayrak <ugurbayrak001@gmail.com>
2024-01-15 13:15:49 +0300

    initial commit

---
+++ app/src/main/java/com/ugurbayrak/cryptoapp/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ugurbayrak.cryptoapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ugurbayrak.cryptoapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
