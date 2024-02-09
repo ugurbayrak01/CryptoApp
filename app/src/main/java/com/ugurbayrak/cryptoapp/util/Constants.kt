c82f748b46d578f6179d10aaa0bdbfea4c97c412
Uğur Bayrak <56801405+ugurbayrak01@users.noreply.github.com>
2024-02-09 23:54:19 +0300

    Update Constants.kt

--- app/src/main/java/com/ugurbayrak/cryptoapp/util/Constants.kt
+++ app/src/main/java/com/ugurbayrak/cryptoapp/util/Constants.kt
@@ -2,6 +2,6 @@ package com.ugurbayrak.cryptoapp.util
 
 object Constants {
     const val BASE_URL = "https://pro-api.coinmarketcap.com"
-    const val API_KEY = "4f72dfe8-1e68-4040-919a-7aae854ca68f"
+    const val API_KEY = "API_KEY"
     const val PREFERENCES_TIME = "preferences_time"
 }
